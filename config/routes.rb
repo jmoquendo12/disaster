f25a341918204d0be2cbef400aa3c99403c234fe
project <>
2022-11-04 14:01:01 +0800

    Change route and input navigation format on devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'welcome#index'
+  root "calamities#index"
+  resources :calamities do
+    resources :comments, except: :show
+  end
+  resources :categories
 end
