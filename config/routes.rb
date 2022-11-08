6c2a30d46ddacfaf001b057e3b49fb0210966eaf
project <>
2022-11-08 16:17:19 +0800

    change path names

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  #
+  # get "0000" => "calamities#index"
   root "calamities#index"
-  resources :calamities do
+  resources :calamities, path_names: {new: "0000", edit: "1111"} do
     resources :comments, except: :show
   end
   resources :categories
