8190ccaffcc82c1cd5bbd982d6f293c670cd3e14
project <>
2022-11-14 11:53:51 +0800

    create database for regions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_055358) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_034714) do
+  create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "calamities", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -20,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_055358) do
     t.datetime "updated_at", null: false
     t.datetime "delete_at"
     t.integer "comments_count", default: 0
+    t.string "unique_id"
+    t.string "ip_address"
     t.index ["user_id"], name: "index_calamities_on_user_id"
   end
 
