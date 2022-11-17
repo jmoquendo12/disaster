3d82d8b181cd6f636b8ad1fd5b9e7657cd54609b
project <>
2022-11-17 14:31:50 +0800

    auto fill province when selected region

--- app/models/address/region.rb
+++ app/models/address/region.rb
@@ -1,4 +1,9 @@
 class Address::Region < ApplicationRecord
   validates :name, presence: true
   validates :code, uniqueness: true
+
+  has_many :city_municipalities
+  has_many :provinces
+  has_many :districts
+  # has_many :calamities, class_name: 'Calamity', foreign_key: 'address_region_id'
 end
--- app/models/province.rb
+++
@@ -1,6 +0,0 @@
-class Province < ApplicationRecord
-  validates :name, presence: true
-  validates :code, uniqueness: true
-
-  belongs_to :region
-end
